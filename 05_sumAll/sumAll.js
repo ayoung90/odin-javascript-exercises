d8665e330d37347339f1d57883c5bcd543ffa378
ayoung90 <ayoung90@live.com.au>
2022-12-01 18:45:28 +1100

    Finished 05_sumall

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    num1 < 0 ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  let start = num1;
+  let end = num2;
+
+  if (num1 > num2) {
+    start = num2;
+    end = num1;
+  }
+  let count = 0;
+  for (i = start; i <= end; i++) {
+    count = count + i;
+  }
+  return count;
 };
 
 // Do not edit below this line
