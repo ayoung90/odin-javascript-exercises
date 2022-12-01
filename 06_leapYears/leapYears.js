1d8cf5021b75892cea8886bfe577f63e097631e8
ayoung90 <ayoung90@live.com.au>
2022-12-01 18:52:48 +1100

    Finished 06_leapyears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0) { // leap year
+    if (year % 100 == 0) { //unless divisible by 100 
+      if (year % 400 == 0) { //however leap year if divisible by 400
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  
+  return false;
 };
 
 // Do not edit below this line
