6a61d07f751ab29bd3ca68b7e8f2120dc0f240f5
ayoung90 <ayoung90@live.com.au>
2022-12-01 19:00:03 +1100

    Finished 07_tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+/*
+ * use Math.round + *10 & /10 to calculate to exactly 1dp
+ */
+
+const convertToCelsius = function (temp) {
+  // (x - 32)* 5/9
+  return Math.round((((temp - 32) * 5) / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  //x * 9/5 + 32
+  return Math.round(((temp * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
