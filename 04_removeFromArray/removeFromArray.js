0058335937651116cbe32dd003f047e34b54d930
ayoung90 <ayoung90@live.com.au>
2022-12-01 17:52:02 +1100

    Completed 04_removefromarray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let array = arguments[0];
+  let argArray = [];
 
+  // Add all remaining arguments to array
+  for (let i = 1; i < arguments.length; i++) {
+    argArray.push(arguments[i]);
+  }
+
+  return array.filter((item) => !argArray.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3], 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
