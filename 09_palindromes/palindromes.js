35770a21c122b70b7e099a4b5f130f4b113f406f
ayoung90 <ayoung90@live.com.au>
2023-05-26 20:58:30 +1000

    Complete exercise. All tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+/**
+ * Determines whether or not a given string is a palindrome
+ * @param {String} input
+ * @returns Boolean
+ */
+const palindromes = function (input) {
+  input = input.toLowerCase();
 
+  let punctuationLess = input.replace(/[^\p{L}\s]/gu, "");
+  let spaceLess = punctuationLess.replace(/\s+/g, "");
+
+  //split into an array to reverse
+  const inputArray = spaceLess.split("");
+  const reverseArray = inputArray.slice().reverse();
+
+  //join to compare input string vs reversed
+  return inputArray.join("") === reverseArray.join("");
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+console.log(palindromes("race. car"));
